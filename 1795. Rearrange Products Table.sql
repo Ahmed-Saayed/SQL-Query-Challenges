71ee35c578244479fd252ff3c0436d53bb00d4c8
Ahmed Sayed <ahmed9pato7@gmail.com>
2025-05-17 04:35:22 +0300

    DONE

---
+++ 1795. Rearrange Products Table.sql
@@ -0,0 +1,24 @@
+select
+      product_id, store1 as price, 'store1' as store
+ from
+      products
+ where
+     store1 is not null
+
+union all
+
+select
+    product_id, store2 as price, 'store2' as store
+from
+    products
+ where
+    store2 is not null
+
+union all
+
+select
+    product_id, store3 as price, 'store3' as store
+from
+    products
+where
+    store3 is not null
