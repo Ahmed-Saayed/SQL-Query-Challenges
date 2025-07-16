226d12893e5a5fb4c68d0f38a79dca0c68a3c970
Ahmed Sayed <ahmed9pato7@gmail.com>
2025-07-16 11:29:40 +0300

    DONE

--- 177. Nth Highest Salary.sql
+++ 177. Nth Highest Salary.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT 
 BEGIN
         DECLARE M INT;
-        SET M = N - 1;
+        SET M = N - 1 ;
   RETURN (
       # Write your MySQL query statement below.
     SELECT (
